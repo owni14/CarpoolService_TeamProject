24b01275ecb3df61c4d2812de9069020947f2050
owni14 <kmwoo514@gmail.com>
2022-06-10 15:22:48 +0900

    예약하기 페이지 - 80% 완성본

--- team_project/src/main/java/com/kh/team/service/MemberService.java
+++ team_project/src/main/java/com/kh/team/service/MemberService.java
@@ -13,6 +13,7 @@ public interface MemberService {
 	public List<Map<String, Object>> getDriverList(String m_company); // 예약하기 페이지 - 운전자 리스트 가져오기 위한 메서드
 	public List<MemberVo> admingetMemberList(PagingDto pagingDto); // 멤버 리스트
 	public int adminGetCount(PagingDto pagingDto); // 관리자 회원관리 페이지 회원명 수 확인
-	public MemberVo getDriverById(String m_id); // 예약하기 페이지 - 운전자 정보 가져오기 위한 메서드
+	public Map<String, Object> getDriverById(String m_id, String m_company); // 예약하기 페이지 - 운전자 정보 가져오기 위한 메서드
 	public boolean submitDriverLicense(String m_id, String ad_license_img); // 운전자 등록
+	public void addPassengerInfo(String m_id, String boardLoct, String boardTime, String driver_seq); // 탑승자 정보 추가
 }

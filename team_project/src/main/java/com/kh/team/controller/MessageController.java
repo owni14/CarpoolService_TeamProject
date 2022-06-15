1fae059e9f5d49602246eafd78be3d53e2074a9e
jaegonLee1 <gin960@naver.com>
2022-06-15 11:17:48 +0900

    [UPDATE] MessageController @Restcontroller -> @Controller [CREATE] send.jsp

--- team_project/src/main/java/com/kh/team/controller/MessageController.java
+++ team_project/src/main/java/com/kh/team/controller/MessageController.java
@@ -3,24 +3,31 @@ package com.kh.team.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.team.service.MessageService;
 import com.kh.team.vo.MessageVo;
 
-
-
-@RestController
+@Controller
 @RequestMapping("/message")
 public class MessageController {
 	
 	@Autowired
 	private MessageService messageService;
 	
+	@RequestMapping(value="/sendMessagePage", method= RequestMethod.GET)
+	public String sendMessagePage()	{
+		System.out.println("send 들어옴");
+		return "/message/send";
+	}
 	
+	
+	@ResponseBody
 	@RequestMapping(value="/notifyComplete", method= RequestMethod.POST)
 	public String notifyComplete(MessageVo messageVo) {
 		System.out.println("messageVo : " + messageVo);
@@ -28,6 +35,7 @@ public class MessageController {
 		return String.valueOf(result);
 	}
 	
+	@ResponseBody
 	@RequestMapping(value="/checkSendedMessage", method= RequestMethod.POST)
 	public List<Integer> checkSended() {
 		List<Integer> list = messageService.adminCheckSendedMessage();
@@ -36,6 +44,7 @@ public class MessageController {
 	}
 	
 	//add event 메세지
+	@ResponseBody
 	@RequestMapping(value="/EventIsNoGet", method= RequestMethod.POST)
 	public String notifyEventIsNoGet(MessageVo messageVo) {
 		System.out.println("messageVo : " + messageVo);
--- team_project/src/main/webapp/WEB-INF/views/message/send.jsp
+++ team_project/src/main/webapp/WEB-INF/views/message/send.jsp
@@ -1,13 +1,37 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-</head>
-<body>
-
-</body>
-</html>
+<%@ include file="/WEB-INF/views/include/header.jsp" %>
+<div class="row">
+	<div class="col-md-2">
+	</div>
+	<div class="col-md-8">
+		<div class="row">
+			<div class="col-md-4">
+				<h1>
+	                <a href="#">쪽지</a>
+	            </h1>
+	            <ul id="NoteBoxMenu">
+	                <li>
+	                    <a href="/notebox/list/4">받은 쪽지함</a>
+	                </li>
+	                <li>
+	                    <a href="/notebox/list/2">보낸 쪽지함</a>
+	                </li>
+	                <li>
+	                    <a href="/notebox/list/5">보관함</a>
+	                </li>
+	                <li>
+	                    <a href="/noteset/rejectlist ">설정</a>
+	                </li>
+	            </ul>
+	            <a href="#">쪽지보내기</a>
+			</div>
+			<div class="col-md-8">
+			</div>
+		</div>
+	</div>
+	<div class="col-md-2">
+	</div>
+</div>
+<%@ include file="/WEB-INF/views/include/footer.jsp" %>

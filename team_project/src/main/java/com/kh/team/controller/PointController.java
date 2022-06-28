de31579645ff1e1dbd161b1c25cce20f460b7e12
jaegonLee1 <gin960@naver.com>
2022-06-28 11:54:36 +0900

    [UPDATE] point페이지 현재 내 포인트 textcolor 흰색으로 수정, 쿠폰 구매시 쪽지 전송 완료

--- team_project/src/main/java/com/kh/team/controller/PointController.java
+++ team_project/src/main/java/com/kh/team/controller/PointController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.team.service.MemberService;
+import com.kh.team.service.MessageService;
 import com.kh.team.service.PointService;
 import com.kh.team.vo.MemberVo;
+import com.kh.team.vo.MessageVo;
 
 @Controller
 @RequestMapping("/point")
@@ -20,6 +22,8 @@ public class PointController {
 	private PointService pointService;
 	@Autowired
 	private MemberService memberService;
+	@Autowired
+	private MessageService messageService;
 	
 	// 포인트 페이지로 이동
 	@RequestMapping(value = "point", method = RequestMethod.GET)
@@ -34,10 +38,19 @@ public class PointController {
 	
 	// 포인트 결제하기
 	@RequestMapping(value = "payment", method = RequestMethod.POST)
-	public String payment(int m_point, RedirectAttributes rttr, HttpSession session) {
+	public String payment(int m_point, String coupon, int price, RedirectAttributes rttr, HttpSession session) {
 		MemberVo loginVo = (MemberVo)session.getAttribute("loginVo");
 		boolean result = pointService.paymet(loginVo.getM_id(), m_point);
-		rttr.addFlashAttribute("payment_result", result);
+		if (result) {
+			MessageVo messageVo = new MessageVo();
+			messageVo.setSender_admin_code("1002");
+			messageVo.setReceiver_m_id(loginVo.getM_id());
+			messageVo.setContent("포인트 스토어에서" + coupon + price + "원권이 도착하였습니다.\n 교환 후 포인트 : " + m_point + "point\n 이용해주셔서 감사합니다.");
+			boolean message_result = messageService.insertNoBlackMessage(messageVo);
+			System.out.println("message_result : " + message_result);
+			rttr.addFlashAttribute("message_result", message_result);
+		}
+		
 		return "redirect:/point/point";
 	}
 	
